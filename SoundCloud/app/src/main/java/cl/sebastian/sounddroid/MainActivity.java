39ec28390c236129f140de6a5ba3fa60cbd22a08
sebalagos90 <sebalegutie@gmail.com>
2015-03-12 16:25:42 -0300

    Reproduce y pausa la musica. Tambien permite cambiar de musica.

--- SoundCloud/app/src/main/java/cl/sebastian/sounddroid/MainActivity.java
+++ SoundCloud/app/src/main/java/cl/sebastian/sounddroid/MainActivity.java
@@ -1,5 +1,7 @@
 package cl.sebastian.sounddroid;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,7 +10,14 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,13 +34,47 @@ public class MainActivity extends ActionBarActivity {
     static final String TAG = "MainActivity";
     List<Track> tracksList;
     TracksAdapter tracksAdapter;
+    private ImageView selectedThumbnail;
+    private TextView selectedTitle;
+    private Toolbar playerToolbar;
+    private MediaPlayer mediaPlayer;
+    private ImageView playerState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar playerToolbar = (Toolbar) findViewById(R.id.player_toolbar);
+        playerToolbar = (Toolbar) findViewById(R.id.player_toolbar);
+        selectedTitle = (TextView) findViewById(R.id.selected_title);
+        selectedThumbnail = (ImageView) findViewById(R.id.selected_thumbnail);
+        playerState = (ImageView) findViewById(R.id.player_state);
+
+        playerState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toogleSongState();
+
+            }
+        });
+
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                toogleSongState();
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                playerState.setImageResource(R.drawable.ic_play);
+            }
+        });
+
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.songsListRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -55,9 +98,40 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+        tracksAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Track selectedTrack = tracksList.get(position);
+                selectedTitle.setText(selectedTrack.getTitle());
+                Picasso.with(MainActivity.this).load(selectedTrack.getAvatarURL()).into(selectedThumbnail);
+
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
+                try {
+                    mediaPlayer.setDataSource(selectedTrack.getStreamURL()+"?client_id="+SoundCloudService.CLIENT_ID);
+                    mediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
 
     }
 
+    private void toogleSongState() {
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            playerState.setImageResource(R.drawable.ic_play);
+        }
+        else{
+            mediaPlayer.start();
+            playerState.setImageResource(R.drawable.ic_pause);
+        }
+    }
 
 
     @Override
@@ -80,5 +154,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            if(mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+            }
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
--- SoundCloud/app/src/main/java/cl/sebastian/sounddroid/soundcloud/TracksAdapter.java
+++ SoundCloud/app/src/main/java/cl/sebastian/sounddroid/soundcloud/TracksAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,12 +22,17 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
 
     List<Track> tracksList;
     Context context;
+    AdapterView.OnItemClickListener mOnItemClickListener;
+
 
     public TracksAdapter(List<Track> tracksList, Context context){
         this.tracksList = tracksList;
         this.context = context;
     }
 
+    public void setOnItemClickListener(AdapterView.OnItemClickListener mOnItemClickListener) {
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
@@ -49,7 +55,7 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
         return tracksList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public final TextView titleTextView;
         public final ImageView thumbImageView;
@@ -57,6 +63,15 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
             super(v);
             titleTextView = (TextView) v.findViewById(R.id.track_title);
             thumbImageView = (ImageView) v.findViewById(R.id.track_thumbnail);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(mOnItemClickListener!=null){
+                mOnItemClickListener.onItemClick(null,v,getPosition(),0);
+            }
+
         }
     }
 }
